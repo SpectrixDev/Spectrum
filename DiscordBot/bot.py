327f3d3b33aa1cdcc304fd6327f95d757a4a37aa
Spectrix <owenhutch123@gmail.com>
2018-06-30 16:37:22 +0200

    I made it good
    Also printing the messages like logs doesn't work anyone know why not? Only works for @bot.listen()

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -4,23 +4,22 @@ import time
 from time import ctime
 import datetime
 import random
-import os
-import sys
 import json
 import aiohttp
 from   discord.ext import commands
 import apiai
+import logging
 
 # Tokens (changed for GitHub obviously, and if I'm stupid and leak any they WILL be regenerated immediatly...)
-bot_run_token = "secret"
-CLIENT_ACCESS_TOKEN = 'secret'
-dbltoken = "secret"
+bot_run_token = "baths are"
+CLIENT_ACCESS_TOKEN = 'l i t e r a l l y'
+dbltoken = "reverse boats..."
 
 bot = commands.Bot(command_prefix="$")
 bot.remove_command("help")
 roast_database = "225" # Current count of roasts in the database. Roast list not on GitHub due to people being able to copy my hard work :(
 presenceGame = ":)"
-typeGame = "3"
+typeGame = random.randint(0,3)
 ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 startup_extensions = ['cogs.ownerCommands',
@@ -36,15 +35,10 @@ startup_extensions = ['cogs.ownerCommands',
 @bot.event
 async def on_ready():
     print("=========\nConnected\n=========\n")
-    print("Current servers: {}".format(len(bot.servers)))
-    print("Time on start: {}".format(datetime.datetime.now()))
-    # DBL Publishing
-    url = "https://discordbots.org/api/bots/" + bot.user.id + "/stats"
-    async with aiohttp.ClientSession() as aioclient:
-        await aioclient.post(url, data={ "server_count": len(bot.servers) }, headers={"Authorization": dbltoken})
-        await bot.change_presence(game=discord.Game(name=("$help on {0} servers!".format(len(bot.servers))),
-                                                        url="https://go.twitch.tv/gdspectrix",
-                                                        type=random.randint(0, 3)))
+    print(f"Current servers: {len(bot.servers)}")
+    print(f"Time on start: {ctime()}")
+    print(f"Current Discord.py version: {discord.__version__}")
+    await bot.change_presence(game=discord.Game(name=(f"{presenceGame} | {len(bot.servers)} servers!"), url=("https://go.twitch.tv/gdspectrix"), type=typeGame))
     print("\nStartup successful. Ready for use!\n")
 
 
@@ -52,24 +46,6 @@ def is_owner(ctx): # Check if the command can be used by the bot owner [ Spectri
         if ctx.message.author.id == "276707898091110400":
             return True
         return False
-
-@bot.event
-async def on_server_join(server):
-    url = "https://discordbots.org/api/bots/" + bot.user.id + "/stats"
-    async with aiohttp.ClientSession() as aioclient:
-        await aioclient.post(url, data={"server_count": len(bot.servers)}, headers={"Authorization": dbltoken})
-        await bot.change_presence(game=discord.Game(name=("$help on {0} servers!".format(len(bot.servers))),
-                                                    url="https://go.twitch.tv/gdspectrix",
-                                                    type=random.randint(0, 3)))
-
-@bot.event
-async def on_server_remove(server):
-    url = "https://discordbots.org/api/bots/" + bot.user.id + "/stats"
-    async with aiohttp.ClientSession() as aioclient:
-        await aioclient.post(url, data={"server_count": len(bot.servers)}, headers={"Authorization": dbltoken})
-        await bot.change_presence(game=discord.Game(name=("$help on {0} servers!".format(len(bot.servers))),
-                                                    url="https://go.twitch.tv/gdspectrix",
-                                                    type=random.randint(0, 3)))
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -80,6 +56,11 @@ async def on_command_error(ctx, error):
             return await ctx.author.send(f"```Error: NotPrivateMessage ({ctx.command} can not be used in Private Messages.)```")
         except:
             pass
+
+@bot.command(name="changelog", pass_context=True)
+async def whatsnew(ctx):
+    await bot.say("""```Nothing big right now, do $help ;)```""")
+    print(f"Showed the changelog. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 @bot.command(pass_context=True)
 async def ping():
@@ -115,30 +96,31 @@ async def ping():
     pingEmbed = discord.Embed(description="Pinged 3 times and calculated the avarage.", colour=discord.Colour(value=0x3be801))
     pingEmbed.set_author(name="Pong!")
     pingEmbed.set_thumbnail(url="https://images.discordapp.net/avatars/320590882187247617/138033611e0989895474ac1e8f61cbb8.png?size=512")
-    pingEmbed.add_field(name="First ping:", value="{0}ms".format(speed1), inline=True)
-    pingEmbed.add_field(name="Second ping:", value="{0}ms".format(speed2), inline=True)
-    pingEmbed.add_field(name="Third ping:", value="{0}ms".format(speed3), inline=True)
+    pingEmbed.add_field(name="First ping:", value=f"{speed1}ms", inline=True)
+    pingEmbed.add_field(name="Second ping:", value=f"{speed2}ms", inline=True)
+    pingEmbed.add_field(name="Third ping:", value=f"{speed3}ms", inline=True)
     pingEmbed.add_field(name="Speed comment:", value=speedComment, inline=True)
-    pingEmbed.add_field(name=">> Average speed: <<", value=">> {0}ms <<".format(speedAverage), inline=False)
-    pingEmbed.set_footer(text="Estimated total time elapsed: {0}ms".format(speed1+speed2+speed3))
+    pingEmbed.add_field(name=">> Average speed: <<", value=f">> {speedAverage}ms <<", inline=False)
+    pingEmbed.set_footer(text=f"Estimated total time elapsed: {speed1+speed2+speed3}ms")
     await bot.delete_message(ping1)
     await bot.say(embed=pingEmbed)
-
-
-@bot.command(name="database", pass_context=True)
-async def database(ctx, databaseType):
-    """Current databases:\nroast""" # lol that's all
-    if databaseType == "roast":
-        await bot.say("**My roast database consists of `{0}` roasts**".format(roast_database))
-    else:
-        await bot.say("```Incorrect input. Try $help database.\nCurrent databases:\nroast```")
-
+    print (f"Ponged with an average speed of {speedAverage}ms. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 @bot.command(name="roast", pass_context=True)
 async def roast(ctx):
     """Insults picked and created by Spectrix specifically for the bot."""
     roast = random.choice(open("RoastList.txt").readlines())
     await bot.say(roast)
+    print(f'Roasted {ctx.message.author.name} with the roast, "{roast}". Server: {message.server.name}. Time: {datetime.datetime.now().time()}')
+
+@bot.command(name="database", pass_context=True)
+async def database(ctx, databaseType):
+    """Current databases:\nroast""" # lol that's all
+    if databaseType == "roast":
+        await bot.say(f"**My roast database consists of `{roast_database}` roasts**")
+    else:
+        await bot.say("```Incorrect input.\nCurrent databases:\nroast```")
+    print(f"Showed the {databaseType} database. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 
 @bot.command(name="poll", pass_context=True)
@@ -146,7 +128,7 @@ async def poll(ctx, *, pollInfo):
     """Creates a simple poll within the server"""
     emb = (discord.Embed(description=pollInfo, colour=0x3be801))
     emb.set_author(name="Add your vote!", icon_url="https://lh3.googleusercontent.com/7ITYJK1YP86NRQqnWEATFWdvcGZ6qmPauJqIEEN7Cw48DZk9ghmEz_bJR2ccRw8aWQA=w300")
-    emb.set_footer(text="Poll created by {}".format(ctx.message.author))
+    emb.set_footer(text=f"Poll created by {ctx.message.author}")
     try:
         await bot.delete_message(ctx.message)
     except discord.Forbidden:
@@ -154,17 +136,16 @@ async def poll(ctx, *, pollInfo):
     pollMessage = await bot.say(embed=emb)
     await bot.add_reaction(pollMessage, "\N{THUMBS UP SIGN}")
     await bot.add_reaction(pollMessage, "\N{THUMBS DOWN SIGN}")
-
+    print(f"Started a poll. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 @bot.command(name="presence", pass_context=True)
 async def presence(ctx, typeGame: int, *, presenceGame):
     """Changes the bot's presence"""
     if ctx.message.author.id == "276707898091110400":
-        await bot.change_presence(game=discord.Game(name=("{0} | {1} servers!".format(presenceGame, len(bot.servers))), url=("https://go.twitch.tv/gdspectrix"), type=typeGame))
+        await bot.change_presence(game=discord.Game(name=(f"{presenceGame} | {len(bot.servers)} servers!"), url=("https://go.twitch.tv/gdspectrix"), type=typeGame))
         await bot.say("Done!")
     else:
         await bot.say("No. (Only Spectrix can do that)")
-
 
 @commands.has_permissions(manage_messages=True)
 @bot.command(name="clear", pass_context=True)
@@ -172,10 +153,11 @@ async def clear(ctx, number):
         try:
             await bot.purge_from(ctx.message.channel, limit = (int(number) + 1))
             await asyncio.sleep(1)
-            clearConfirmation = await bot.say("**Cleared `{}` messages from this channel**".format(number))
+            clearConfirmation = await bot.say(f"**Cleared `{number}` messages from this channel**")
             await bot.add_reaction(clearConfirmation, "\N{OK HAND SIGN}")
             await asyncio.sleep(4)
             await bot.delete_message(clearConfirmation)
+            print(f"Purged {number} messages. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
         except discord.Forbidden:
             await bot.say("```I seem to have missing permissions. I need the manage_message permission to preform this action.```")
@@ -185,36 +167,30 @@ async def clear(ctx, number):
 async def invite(ctx):
     await bot.send_message(ctx.message.author, "**https://bit.ly/SpectrumDiscord**\n*Here's my invite link!*")
     await bot.say("**I sent you the invite link in your DMs :mailbox_with_mail:**")
+    await bot.add_reaction(clearConfirmation, "\N{OK HAND SIGN}")
+    print(f"Gave my invite link to {ctx.message.author.name}. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 
 @bot.command(name="support", pass_context=True)
 async def support(ctx):
     await bot.send_message(ctx.message.author, "**https://discord.gg/ecXdjTD/**\n*Here's my official server! *")
     await bot.say("**I sent you the server invite in your DMs :mailbox_with_mail:**")
-
+    await bot.add_reaction(clearConfirmation, "\N{THUMBS UP SIGN}")
+    print(f"Gave my server invite to {ctx.message.author.name}. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 @bot.command(name="server", pass_context=True)
 async def server(ctx):
     await bot.send_message(ctx.message.author, "**https://discord.gg/ecXdjTD/**\n*Here's my official server!*")
     await bot.say("**I sent you the server invite in your DMs :mailbox_with_mail:**")
-
+    await bot.add_reaction(clearConfirmation, "\N{THUMBS UP SIGN}")
+    print(f"Gave my server invite to {ctx.message.author.name}. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 @bot.command(name="help", pass_context=True)
 async def help(ctx):
     await bot.send_message(ctx.message.author, "**https://spectrix.pythonanywhere.com/spectrum**\n*Here's my help page!*")
     await bot.say("**I sent you help in your DMs :mailbox_with_mail:**")
-
-
-@bot.command(name="changelog", pass_context=True)
-async def whatsnew(ctx):
-    await bot.say("""```Finally, I have released SpectrumV2. I can actually be proud of this version. Anyways, here's some new stuff:\n\n
-    The bot is completely rewritten in discord.ext (Python)\n
-    New commands include SpectrumPhone™, currentgames, whosplaying, me_irl, bigemote, zalgo, userinfo, serverinfo, botinfo, clear, and more! (Do $help for more information)\n\n
-    All commands actually work now, such as $ship\n
-    Improved roasts, showerthoughts (now updated hourly), 8ball (ew), poll, ping, botinfo and basiclly everything else :)\n
-    Removed stupid commands as well\n\n
-    Also, rewritten the whole chatbot. It needs to learn a bit, so be patient :)\n
-    And now I'm actually gonna work on the bot!```""")
+    await bot.add_reaction(clearConfirmation, "\N{OK HAND SIGN}")
+    print(f"Helped the user {ctx.message.author.name}. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
 
 @bot.listen()
@@ -228,14 +204,12 @@ async def on_message(message):
                 user_message = message.content.replace(message.server.me.mention,'') if message.server else message.content
 
                 request = ai.text_request()
-                # will hopefully update to dialogflowV2 soon, not api.ai
                 request.query = user_message
 
                 response = json.loads(request.getresponse().read())
 
                 result = response['result']
                 action = result.get('action')
-                actionIncomplete = result.get('actionIncomplete', False)
 
                 if action == "user.requests.help":
                     await bot.send_message(message.author, "**https://spectrix.pythonanywhere.com/spectrum**\n*Here's my help page!*")
@@ -248,14 +222,17 @@ async def on_message(message):
                 else:
                     await bot.send_message(message.channel, f"{message.author.mention} {response['result']['fulfillment']['speech']}")
 
-                print ("Chatted with a user on the server: {0}. Time: {1}".format((message.server.name), datetime.datetime.now().time()))
+                print(f"Chatted with a user. Server: {message.server.name}. Time: {datetime.datetime.now().time()}")
 
         except KeyError:
-            await bot.send_message(message.channel, "`Error: 'KeyError', make sure you gave not too little input and not too much ;)`")
+            await bot.send_message(message.channel, "```Error: 'KeyError', make sure you gave not too little input and not too much ;)```")
+        except AttributeError:
+            print(f"Chatted with a user in DMs. Time: {datetime.datetime.now().time()}")
 
 
 if __name__ == '__main__':
-
+    global logger
+    logger = logging.getLogger('bot')
     for extension in startup_extensions:
         bot.load_extension(extension)
 
