d6d1ec6e7d84bfb201bb357819c87e5fa57e30e3
Spectrix <owenhutch123@gmail.com>
2018-08-18 17:30:32 +0200

    less lines = better code

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -11,11 +11,9 @@ class Moderation:
     @commands.command()
     async def clear(self, ctx, number):
         try:
+            msg = "message"
             if number = 1:
-                msg = "message"
-            else:
-                msg = "messages"
-
+                msg+='s'
             amt = await ctx.channel.purge(limit = (int(number) + 1))
             await asyncio.sleep(1)
             clearConfirmation = await ctx.send(f"**Cleared `{len(amt) - 1}` {msg} from this channel**", delete_after=4.0)
