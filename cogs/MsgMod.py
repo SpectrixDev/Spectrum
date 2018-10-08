f8c198ecde391e8a8cc83f2e98e0cce73a1ca6ed
SpectrixOfficial <owenhutch123@gmail.com>
2018-10-08 09:07:13 +0200

    Added slowmode

--- cogs/MsgMod.py
+++ cogs/MsgMod.py
@@ -16,5 +16,24 @@ class MsgModeration:
         clearConfirmation = await ctx.send(f"**Cleared `{len(amt) - 1}` {msg} from this channel**", delete_after=4.0)
         await clearConfirmation.add_reaction("a:SpectrumOkSpin:466480898049835011")
 
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    @commands.command(aliases=['slowmo'])
+    async def slowmode(self, ctx, seconds: int=0):
+        if seconds > 120:
+            return await ctx.send(":no_entry: Amount can't be over 120 seconds")
+        if seconds == 0:
+            await ctx.channel.edit(slowmode_delay=seconds)
+            a = await ctx.send("**Slowmode is off for this channel**")
+            await a.add_reaction("a:SpectrumOkSpin:466480898049835011")
+        else:
+            if seconds == 1:
+                numofsecs = "second"
+            else:    
+                numofsecs = "seconds"
+            await ctx.channel.edit(slowmode_delay=(seconds))
+            confirm = await ctx.send(f"**Set the channel slow mode delay to `{seconds}` {numofsecs}\nTo turn this off, do $slowmode**")
+            await confirm.add_reaction("a:SpectrumOkSpin:466480898049835011")
+
 def setup(bot):
     bot.add_cog(MsgModeration(bot))
