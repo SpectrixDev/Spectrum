c0a001894deb4bb4a568ad1a9c804fb61b21c8ea
SpectrixOfficial <owenhutch123@gmail.com>
2018-10-17 16:13:33 +0200

    Added specrypt

--- cogs/TextConverters.py
+++ cogs/TextConverters.py
@@ -18,7 +18,7 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-    
+
     @commands.command()
     async def expand(self, ctx,  num: int, *, s: clean_content):
         spacing = ""
@@ -36,7 +36,7 @@ class TextConverters:
                     await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
         else:
             await ctx.send("```fix\nError: The number can only be from 1 to 5```")
-    
+
     @commands.command()
     async def reverse(self, ctx, *, s: clean_content):
         result = await commands.clean_content().convert(ctx, s[::-1])
@@ -48,7 +48,7 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-    
+
     @commands.command()
     async def texttohex(self, ctx, *, s):
         try:
@@ -63,7 +63,7 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-    
+
     @commands.command()
     async def hextotext(self, ctx, *, s):
         try:
@@ -78,7 +78,7 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-    
+
     @commands.command()
     async def texttobinary(self, ctx, *, s):
         try:
@@ -93,7 +93,7 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-    
+
     @commands.command()
     async def binarytotext(self, ctx, *, s):
         try:
@@ -108,7 +108,47 @@ class TextConverters:
                 await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
             except Exception:
                 await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
-        
 
-def setup(bot): 
+    @commands.group()
+    async def specrypt(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("```fix\nInvalid input. Please use one of the following:\nencrypt (e)\ndecrypt (d)\n\nExample: $specrypt e Hello world!```")
+
+    @specrypt.command(aliases=['e'])
+    async def encrypt(self, ctx, *, s):
+        a = ''
+        try:
+            for letter in s:
+                a+=chr(ord(letter)+len(s))
+            cleanS = await commands.clean_content().convert(ctx, a)
+        except Exception as e:
+            await ctx.send(f"**Error: `{e}`. This probably means the input is malformed. Sorry, I'm not perfect and my creator is dumb**")
+        if len(cleanS) <= 479:
+            await ctx.send(f"```{cleanS}```")
+        else:
+            try:
+                await ctx.author.send(f"```{cleanS}```")
+                await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
+            except Exception:
+                await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
+
+    @specrypt.command(aliases=['d'])
+    async def decrypt(self, ctx, *, s):
+        a = ''
+        try:
+            for letter in s:
+                a+=chr(ord(letter)-len(s))
+            cleanS = await commands.clean_content().convert(ctx, a)
+        except Exception as e:
+            await ctx.send(f"**Error: `{e}`. This probably means the input is malformed. Sorry, I'm not perfect and my creator is dumb**")
+        if len(cleanS) <= 479:
+            await ctx.send(f"```{cleanS}```")
+        else:
+            try:
+                await ctx.author.send(f"```{cleanS}```")
+                await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
+            except Exception:
+                await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
+
+def setup(bot):
     bot.add_cog(TextConverters(bot))
