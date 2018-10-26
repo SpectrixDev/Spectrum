074ed8d89bafb84bfcf1de11e54fbe0a1333f875
EnterNewName <lifez.memes@gmail.com>
2018-10-25 19:07:04 -0500

    added uptime to an module

--- cogs/General.py
+++ cogs/General.py
@@ -38,6 +38,17 @@ class General:
         await msg.edit(content=f":ping_pong: **{round((round(sum(times)) + round(self.bot.latency * 1000))/4)}ms**", embed=embed)
 
     @commands.command()
+    async def uptime(self, ctx):
+        file = open('database/uptime.json', "r")
+        time = json.load(file)['uptimestats']
+        uptimeraw = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+        uptime = datetime.datetime.utcnow() - uptimeraw
+        hours, remainder = divmod(int(uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
+
+    @commands.command()
     async def roast(self, ctx):
         await ctx.send(random.choice(open("databases/RoastList.txt").readlines()))
 
