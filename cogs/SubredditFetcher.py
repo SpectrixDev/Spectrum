3bd63b4ca932e0b0f0e58d6d3301f0a26c609aa9
Spectrix <owenhutch123@gmail.com>
2020-10-08 08:32:44 +0200

    Made ctx.typing() more efficient

--- cogs/SubredditFetcher.py
+++ cogs/SubredditFetcher.py
@@ -11,40 +11,41 @@ memeSubreddits = ["BikiniBottomTwitter", "memes", "2meirl4meirl", "deepfriedmeme
 
 async def getSub(self, ctx, sub):
         """Get stuff from requested sub"""
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.reddit.com/r{sub}/hot.json?limit=100") as response:
-                request = await response.json()
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"https://www.reddit.com/r{sub}/hot.json?limit=100") as response:
+                    request = await response.json()
 
-        attempts = 1
-        while attempts < 5:
-            if 'error' in request:
-                print("failed request {}".format(attempts))
-                await asyncio.sleep(2)
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(f"https://www.reddit.com/r/{sub}/hot.json?limit=100") as response:
-                        request = await response.json()
-                attempts += 1
-            else:
-                index = 0
+            attempts = 1
+            while attempts < 5:
+                if 'error' in request:
+                    print("failed request {}".format(attempts))
+                    await asyncio.sleep(2)
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(f"https://www.reddit.com/r/{sub}/hot.json?limit=100") as response:
+                            request = await response.json()
+                    attempts += 1
+                else:
+                    index = 0
 
-                for index, val in enumerate(request['data']['children']):
-                    if 'url' in val['data']:
-                        url = val['data']['url']
-                        urlLower = url.lower()
-                        accepted = False
-                        for j, v, in enumerate(acceptableImageFormats): #check if it's an acceptable image
-                            if v in urlLower:
-                                accepted = True
-                        if accepted:
-                            if url not in memeHistory:
-                                memeHistory.append(url)  #add the url to the history, so it won't be posted again
-                                if len(memeHistory) > 63: #limit size
-                                    memeHistory.popleft() #remove the oldest
+                    for index, val in enumerate(request['data']['children']):
+                        if 'url' in val['data']:
+                            url = val['data']['url']
+                            urlLower = url.lower()
+                            accepted = False
+                            for j, v, in enumerate(acceptableImageFormats): #check if it's an acceptable image
+                                if v in urlLower:
+                                    accepted = True
+                            if accepted:
+                                if url not in memeHistory:
+                                    memeHistory.append(url)  #add the url to the history, so it won't be posted again
+                                    if len(memeHistory) > 63: #limit size
+                                        memeHistory.popleft() #remove the oldest
 
-                                break #done with this loop, can send image
-                await ctx.send(memeHistory[len(memeHistory) - 1]) #send the last image
-                return
-        await ctx.send("_{}! ({})_".format(str(request['message']), str(request['error'])))
+                                    break #done with this loop, can send image
+                    await ctx.send(memeHistory[len(memeHistory) - 1]) #send the last image
+                    return
+            await ctx.send("_{}! ({})_".format(str(request['message']), str(request['error'])))
 
 class SubredditFetcher(commands.Cog):
     def __init__(self, bot):
@@ -133,27 +134,22 @@ class SubredditFetcher(commands.Cog):
     
     @commands.command(aliases=['dankmeme', 'dank'])
     async def dankmemes(self, ctx):
-      async with ctx.typing():
         await getSub(self, ctx, 'dankmemes')
         
     @commands.command()
     async def me_irl(self, ctx):
-      async with ctx.typing():
         await getSub(self, ctx, 'me_irl')
 
     @commands.command()
     async def programmerhumor(self, ctx):
-      async with ctx.typing():
         await getSub(self, ctx, 'ProgrammerHumor')
 
     @commands.command()
     async def surrealmemes(self, ctx):
-      async with ctx.typing():
         await ctx.send("**This command has been moved to another bot due to popular demand. You can find that bot here:** https://discordbots.org/bot/532917889926299648")
     
     @commands.command(aliases=['hm', 'hmm', 'hmmmm', 'hmmmmm'])
     async def hmmm(self, ctx):
-      async with ctx.typing():
         await ctx.send("**This command has been moved to another bot due to popular demand. You can find that bot here:** https://discordbots.org/bot/532917889926299648")
 
 def setup(bot):
